c41af572929c2f1706b40e2c7fad0bf1488e9ff4
alecsia <comanalecsiaa@gmail.com>
2024-12-09 15:04:36 +0200

    initial configuration

--- src/main/java/com/example/web/controller/AboutController.java
+++ src/main/java/com/example/web/controller/AboutController.java
@@ -1,12 +1,23 @@
 package com.example.web.controller;
 
+import com.example.web.models.TeamMember;
+import com.example.web.services.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AboutController {
+    @Autowired
+    private TeamService teamService;
+
     @GetMapping("/about")
-    public String showAboutPage(){
+    public String showAboutPage(Model model){
+        List<TeamMember> teamMembers = teamService.getTeamMembers();
+        model.addAttribute("teamMembers", teamMembers);
         return "about";
     }
 }
