c1c6c1c47308d9989be1d02f5db6dbee5fdbbc23
alecsia <comanalecsiaa@gmail.com>
2025-01-05 12:45:08 +0200

    change gallery photo

--- src/main/java/com/example/web/controller/EventController.java
+++ src/main/java/com/example/web/controller/EventController.java
@@ -1,8 +1,5 @@
 package com.example.web.controller;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
